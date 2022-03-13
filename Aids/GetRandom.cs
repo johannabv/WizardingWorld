5f762e4647607b76f7ecf762e9593cb6e5d48300
johannabv <johannabrigite.verre@gmail.com>
2022-03-13 17:15:10 +0200

    GetRandom testid int ja double

--- Aids/GetRandom.cs
+++ Aids/GetRandom.cs
@@ -1,8 +1,24 @@
 ﻿
 namespace WizardingWorld.Aids {
     public class GetRandom {
-        public static int Int32(int min = int.MinValue, int max = int.MaxValue) => Random.Shared.Next(min, max);
-        public static double Double(double min = short.MinValue, double max = short.MaxValue) => min + Random.Shared.NextDouble() * (max - min);
+        private static void PutMinFirst<T>(ref T min, ref T max) where T : IComparable<T> {
+            if (min.CompareTo(max) < 0) return;
+            var v = min;
+            min = max;
+            max = v;
+        }
+        public static int Int32(int? min = null, int? max = null) {
+            var minValue = min ?? -1000;
+            var maxValue = max ?? 1000;
+            PutMinFirst(ref minValue, ref maxValue);
+            return Random.Shared.Next(minValue, maxValue);
+        }
+        public static double Double(double? min = null, double? max = null) {
+            var minValue = min ?? -1000.0;
+            var maxValue = max ?? 1000.0;
+            PutMinFirst(ref minValue, ref maxValue); 
+            return minValue + Random.Shared.NextDouble() * (maxValue - minValue);
+        }
         public static char Char(char min = char.MinValue, char max = char.MaxValue) => (char)Int32(min, max);
         public static bool Bool() => Int32() % 2 == 0;
         public static DateTime DateTime(ushort minYear = 1900, ushort maxYear = 2100)
@@ -20,9 +36,12 @@ namespace WizardingWorld.Aids {
             for (var i = 0; i < length; i++) s += Char('a', 'z');
             return s;
         }
-        public static dynamic Value<T>() {
+        public static dynamic Value<T>(T? min = default, T? max = default) {
             if (typeof(T) == typeof(bool) || typeof(T) == typeof(bool?)) return Bool();
             else if (typeof(T) == typeof(DateTime) || typeof(T) == typeof(DateTime?)) return DateTime();
+            else if (typeof(T) == typeof(double) || typeof(T) == typeof(double?)) return Double(Convert.ToDouble(min), Convert.ToDouble(max));
+            else if (typeof(T) == typeof(char) || typeof(T) == typeof(char?)) return Char(Convert.ToChar(min), Convert.ToChar(max));
+            else if (typeof(T) == typeof(int) || typeof(T) == typeof(int?)) return Int32(Convert.ToInt32(min), Convert.ToInt32(max));
             else return String();
         }
     }
--- Tests/Aids/GetRandomTests.cs
+++ Tests/Aids/GetRandomTests.cs
@@ -1,5 +1,41 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using WizardingWorld.Aids;
 
 namespace Tests.Aids {
-    [TestClass] public class GetRandomTests : IsTypeTested { }
+    [TestClass] public class GetRandomTests : IsTypeTested {
+        private void TestForNumbers<T>(T min, T max) where T : IComparable<T>{
+            var x = GetRandom.Value(min, max);
+            var y = GetRandom.Value(min,max);
+            IsInstanceofType(x, typeof(T));
+            IsInstanceofType(y, typeof(T));
+            IsTrue(x >= (min.CompareTo(max) < 0 ? min : max));
+            IsTrue(y >= (min.CompareTo(max) < 0 ? min : max));
+            IsTrue(x <= (min.CompareTo(max) < 0 ? max : min));
+            IsTrue(y <= (min.CompareTo(max) < 0 ? max : min));
+            AreNotEqual(x, y);
+        }
+
+        [DataRow(-1000, 1000)] 
+        [DataRow(-1000, 0)]
+        [DataRow(0, 1000)]
+        [DataRow(int.MaxValue - 100, int.MaxValue)]
+        [DataRow(int.MinValue, int.MinValue + 100)]
+        [DataRow(1000, -1000)]
+        [TestMethod] public void Int32Test(int min, int max) => TestForNumbers<int>(min,max);
+
+        [DataRow(-1000.0, 1000.0)] 
+        [DataRow(-1000.0, 0.0)]
+        [DataRow(0.0, 1000.0)]
+        [DataRow(double.MaxValue - 1.0E+308, double.MaxValue)]
+        [DataRow(double.MinValue, double.MinValue + 1.0E+308)]
+        [DataRow(1000.0, -1000.0)]
+        [TestMethod] public void DoubleTest(double min, double max) => TestForNumbers<double>(min,max);
+        
+        [TestMethod] public void CharTest() => IsInconclusive();
+        [TestMethod] public void BoolTest() => IsInconclusive();
+        [TestMethod] public void DateTimeTest() => IsInconclusive();
+        [TestMethod] public void StringTest() => IsInconclusive();
+        [TestMethod] public void ValueTest() => IsInconclusive();
+    }
 }
