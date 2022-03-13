68d9c70b3e2fb447857790d21a09779d85e69bcf
johannabv <johannabrigite.verre@gmail.com>
2022-03-13 18:29:45 +0200

    GetRandom CharsTest

--- Tests/Aids/GetRandomTests.cs
+++ Tests/Aids/GetRandomTests.cs
@@ -4,7 +4,7 @@ using WizardingWorld.Aids;
 
 namespace Tests.Aids {
     [TestClass] public class GetRandomTests : IsTypeTested {
-        private void TestForNumbers<T>(T min, T max) where T : IComparable<T>{
+        private void Test<T>(T min, T max) where T : IComparable<T>{
             var x = GetRandom.Value(min, max);
             var y = GetRandom.Value(min,max);
             IsInstanceofType(x, typeof(T));
@@ -22,7 +22,7 @@ namespace Tests.Aids {
         [DataRow(int.MaxValue - 100, int.MaxValue)]
         [DataRow(int.MinValue, int.MinValue + 100)]
         [DataRow(1000, -1000)]
-        [TestMethod] public void Int32Test(int min, int max) => TestForNumbers<int>(min,max);
+        [TestMethod] public void Int32Test(int min, int max) => Test<int>(min,max);
 
         [DataRow(-1000.0, 1000.0)] 
         [DataRow(-1000.0, 0.0)]
@@ -30,10 +30,23 @@ namespace Tests.Aids {
         [DataRow(double.MaxValue - 1.0E+308, double.MaxValue)]
         [DataRow(double.MinValue, double.MinValue + 1.0E+308)]
         [DataRow(1000.0, -1000.0)]
-        [TestMethod] public void DoubleTest(double min, double max) => TestForNumbers<double>(min,max);
-        
-        [TestMethod] public void CharTest() => IsInconclusive();
-        [TestMethod] public void BoolTest() => IsInconclusive();
+        [TestMethod] public void DoubleTest(double min, double max) => Test<double>(min,max);
+
+        [DataRow(char.MinValue, char.MaxValue)]
+        [DataRow('a', 'z')]
+        [DataRow('1', '9')]
+        [DataRow('0','L')]
+        [TestMethod] public void CharTest(char min, char max) => Test<char>(min, max);
+        [TestMethod] public void BoolTest() {
+            var x = GetRandom.Bool();
+            var y = GetRandom.Bool();
+            var i = 0;
+            while (x == y) {
+                y = GetRandom.Bool();
+                if (i == 5) AreNotEqual(x, y);
+                i++;
+            }
+        }
         [TestMethod] public void DateTimeTest() => IsInconclusive();
         [TestMethod] public void StringTest() => IsInconclusive();
         [TestMethod] public void ValueTest() => IsInconclusive();
