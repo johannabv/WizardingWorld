21bb495835b6c1e9894dfe6949e462e1ae7357e4
johannabv <johannabrigite.verre@gmail.com>
2022-04-03 17:26:56 +0300

    Lehekülje hoidmine

--- Pages/BasePage.cs
+++ Pages/BasePage.cs
@@ -14,38 +14,38 @@ namespace WizardingWorld.Pages {
         [BindProperty] public TView? Item { get; set; }
         public IList<TView>? Items { get; set; }
         public BasePage(TRepo r) => repo = r;
-        public IActionResult OnGetCreate() => Page();
+        public virtual IActionResult OnGetCreate(int pageIndex = 0, string currentFilter = null, string sortOrder = null) => Page();
         public string ItemId => Item?.ID ?? string.Empty;
-        public async Task<IActionResult> OnPostCreateAsync() {
+        public virtual async Task<IActionResult> OnPostCreateAsync(int pageIndex = 0, string currentFilter = null, string sortOrder = null) {
             if (!ModelState.IsValid) return Page();
             await repo.AddAsync(ToObject(Item));
             return RedirectToPage("./Index", "Index");
         }
-        public async Task<IActionResult> OnGetDetailsAsync(string id) {
+        public virtual async Task<IActionResult> OnGetDetailsAsync(string id, int pageIndex = 0, string currentFilter = null, string sortOrder = null) {
             Item = await GetItem(id);
             return Item == null ? NotFound() : Page();
         }
-        public async Task<IActionResult> OnGetDeleteAsync(string id) {
+        public virtual async Task<IActionResult> OnGetDeleteAsync(string id, int pageIndex = 0, string currentFilter = null, string sortOrder = null) {
             Item = await GetItem(id);
             return Item == null ? NotFound() : Page();
         }
-        public async Task<IActionResult> OnPostDeleteAsync(string id) {
+        public virtual async Task<IActionResult> OnPostDeleteAsync(string id, int pageIndex = 0, string currentFilter = null, string sortOrder = null) {
             if (id == null) return NotFound();
             await repo.DeleteAsync(id);
             return RedirectToPage("./Index", "Index");
         }
-        public async Task<IActionResult> OnGetEditAsync(string id) {
+        public virtual async Task<IActionResult> OnGetEditAsync(string id, int pageIndex = 0, string currentFilter = null, string sortOrder = null) {
             Item = await GetItem(id);
             return Item == null ? NotFound() : Page();
         }
-        public async Task<IActionResult> OnPostEditAsync() {
+        public virtual async Task<IActionResult> OnPostEditAsync(int pageIndex = 0, string currentFilter = null, string sortOrder = null) {
             if (!ModelState.IsValid) return Page();
             var obj = ToObject(Item);
             var updated = await repo.UpdateAsync(obj);
             if (!updated) return NotFound();
             return RedirectToPage("./Index", "Index");
         }
-        public async virtual Task<IActionResult> OnGetIndexAsync(int pageIndex = 0, string currentFilter = null, string sortOrder = null) {
+        public virtual async Task<IActionResult> OnGetIndexAsync(int pageIndex = 0, string currentFilter = null, string sortOrder = null) {
             var list = await repo.GetAsync();
             Items = new List<TView>();
             foreach (var obj in list) { 
--- Pages/PagedPage.cs
+++ Pages/PagedPage.cs
@@ -17,6 +17,34 @@ namespace WizardingWorld.Pages {
         public int TotalPages => repo.TotalPages;
         public bool HasNextPage  => repo.HasNextPage;
         public bool HasPreviousPage  => repo.HasPreviousPage;
+        public IActionResult OnGetCreate(int pageIndex = 0, string currentFilter = null, string sortOrder = null) {
+            PageIndex = pageIndex;
+            return base.OnGetCreate(pageIndex, currentFilter, sortOrder);
+        }
+        public override async Task<IActionResult> OnPostCreateAsync(int pageIndex = 0, string currentFilter = null, string sortOrder = null) {
+            PageIndex = pageIndex;
+            return await base.OnPostCreateAsync(pageIndex, currentFilter, sortOrder);
+        }
+        public override async Task<IActionResult> OnGetDetailsAsync(string id, int pageIndex = 0, string currentFilter = null, string sortOrder = null) {
+            PageIndex = pageIndex;
+            return await base.OnGetDetailsAsync(id, pageIndex, currentFilter, sortOrder);
+        }
+        public override async Task<IActionResult> OnGetDeleteAsync(string id, int pageIndex = 0, string currentFilter = null, string sortOrder = null) {
+            PageIndex = pageIndex;
+            return await base.OnGetDeleteAsync(id, pageIndex, currentFilter, sortOrder);
+        }
+        public override async Task<IActionResult> OnPostDeleteAsync(string id, int pageIndex = 0, string currentFilter = null, string sortOrder = null) {
+            PageIndex = pageIndex;
+            return await base.OnPostDeleteAsync(id, pageIndex, currentFilter, sortOrder);
+        }
+        public override async Task<IActionResult> OnGetEditAsync(string id, int pageIndex = 0, string currentFilter = null, string sortOrder = null) {
+            PageIndex = pageIndex;
+            return await base.OnGetEditAsync(id, pageIndex, currentFilter, sortOrder);
+        }
+        public override async Task<IActionResult> OnPostEditAsync(int pageIndex = 0, string currentFilter = null, string sortOrder = null) {
+            PageIndex = pageIndex;
+            return await base.OnPostEditAsync(pageIndex, currentFilter, sortOrder);
+        }
         public async override Task<IActionResult> OnGetIndexAsync(int pageIndex = 0, string currentFilter = null, string sortOrder = null) {  
             PageIndex=pageIndex;
             return await base.OnGetIndexAsync(pageIndex, currentFilter, sortOrder);
