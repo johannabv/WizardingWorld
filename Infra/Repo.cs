8e6e0e50bf59a5c6dae89efdd772b8d24784a49f
johannabv <99389409+johannabv@users.noreply.github.com>
2022-05-05 15:05:41 +0300

    OrderedRepoTests valma

--- Infra/Repo.cs
+++ Infra/Repo.cs
@@ -5,6 +5,5 @@ using WizardingWorld.Domain;
 namespace WizardingWorld.Infra.Party {
     public abstract class Repo<TDomain, TData> : PagedRepo<TDomain, TData> where TDomain : BaseEntity<TData>, new() where TData : BaseData, new() {
         protected Repo(DbContext? c, DbSet<TData>? s) : base(c,s) { }
-        internal protected static bool DoesContain(string? value, string? subValue) => (subValue is null) || (value?.Contains(subValue) ?? false);
     }
 }
--- Tests/Infra/OrderedRepoTests.cs
+++ Tests/Infra/OrderedRepoTests.cs
@@ -1,15 +1,66 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Tests;
+using WizardingWorld.Aids;
 using WizardingWorld.Data.Party;
+using WizardingWorld.Domain;
 using WizardingWorld.Domain.Party;
 using WizardingWorld.Infra;
 
 namespace WizardingWorld.Tests.Infra {
     [TestClass] public class OrderedRepoTests
-        : AbstractClassTests<OrderedRepo<Character, CharacterData>, object> {
+        : AbstractClassTests<OrderedRepo<Character, CharacterData>, FilteredRepo<Character,CharacterData>> {
+        private class TestClass : OrderedRepo<Character, CharacterData> {
+            public TestClass(DbContext? c, DbSet<CharacterData>? s) : base(c, s) { }
+            protected internal override Character ToDomain(CharacterData d) => new(d);
+        }
         protected override OrderedRepo<Character, CharacterData> CreateObj() {
-            throw new NotImplementedException();
+            var db = GetRepo.Instance<WizardingWorldDb>();
+            var set = db?.Characters;
+            return new TestClass(db, set);
+        }
+        [TestMethod] public void CurrentOrderTest() => IsProperty<string>();
+        [TestMethod] public void DescendingStringTest() => AreEqual("_desc", TestClass.DescendingString);
+        
+        [DataRow(null, true)]
+        [DataRow(null, false)]
+        [DataRow(nameof(Character.ID), true)]
+        [DataRow(nameof(Character.ID), false)]
+        [DataRow(nameof(Character.FirstName), true)]
+        [DataRow(nameof(Character.FirstName), false)]
+        [DataRow(nameof(Character.LastName), true)]
+        [DataRow(nameof(Character.LastName), false)]
+        [DataRow(nameof(Character.Gender), true)]
+        [DataRow(nameof(Character.Gender), false)]
+        [DataRow(nameof(Character.DoB), true)]
+        [DataRow(nameof(Character.DoB), false)]
+        [DataRow(nameof(Character.Organisation), true)]
+        [DataRow(nameof(Character.Organisation), false)]
+        [DataRow(nameof(Character.HogwartsHouse), true)]
+        [DataRow(nameof(Character.HogwartsHouse), false)]
+        [TestMethod] public void CreateSQLTest(string str, bool isDescending) {
+            obj.CurrentOrder = (str is null) ? str : isDescending ? str + TestClass.DescendingString : str;
+            var q = obj.CreateSQL();
+            string? actual = q.Expression.ToString();
+            if (str is null) IsTrue(actual.EndsWith(".Select(s => s)"));
+            else if (isDescending) IsTrue(actual.EndsWith(
+                $".Select(s => s).OrderByDescending(x => Convert(x.{str}, Object))"));
+            else IsTrue(actual.EndsWith(
+                $".Select(s => s).OrderBy(x => Convert(x.{str}, Object))"));
+        }
+        
+        [DataRow(true, true)]
+        [DataRow(false, true)]
+        [DataRow(true, false)]
+        [DataRow(false,false)]
+        [TestMethod] public void SortOrderTest(bool isDescending, bool isSame) { 
+            var s = GetRandom.String();
+            var c = isSame ? s : GetRandom.String();
+            obj.CurrentOrder = isDescending ? c + TestClass.DescendingString : c;
+            var actual = obj.SortOrder(s);
+            var sDes = s + TestClass.DescendingString;
+            var expected = isSame ? (isDescending ? s : sDes) : sDes;
+            AreEqual(expected, actual);
         }
     }
 }
